84178439fb42c8c6411be928e3f21ecf47c72193
MC Villoso <mcvilloso@gmail.com>
2022-12-05 20:57:28 -0800

    Added displayCurrentQuestion and startGame function

--- js/game.js
+++ js/game.js
@@ -1,38 +1,38 @@
 // DECLARE 'countdown' variable
+var countdown = (0);
 
 // DECLARE the 'question' array 
     // DECLARE a variable to store the index in the 'question' array
 var questionPosition = 0;
-var question = [
+var questionArray = [
     {
         question: "Commonly used data types DO NOT include:",
-        answer: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-        correctAnswer: "3. alerts"
+        answer: ["A. strings", "B. booleans", "C. alerts", "D. numbers"],
+        correctAnswer: "C. alerts"
     },
     {
         question: "The condition in an if / else statement is enclosed within:",
-        answer: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-        correctAnswer: "3. parentheses",
+        answer: ["A. quotes", "B. curly brackets", "C. parentheses", "D. square brackets"],
+        correctAnswer: "C. parentheses",
     },
     {
         question: "Arrays in JavaScript can be used to store:",
-        answer: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-        correctAnswer: "4. all of the above",
+        answer: ["A. numbers and strings", "B. other arrays", "C. booleans", "D. all of the above"],
+        correctAnswer: "D. all of the above",
     },
     {
         question: "String values must be enclosed within ____ when being assigned to variables.",
-        answer: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-        correctAnswer: "3. quotes",
+        answer: ["A. commas", "B. curly brackets", "C. quotes", "D. parentheses"],
+        correctAnswer: "C. quotes",
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answer: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"],
-        correctAnswer: "4. console log",
+        answer: ["A. JavaScript", "B. terminal/bash", "C. for loops", "D. console log"],
+        correctAnswer: "D. console log",
     },
 ];
 
-// Grabs current question
-var currentQuestion = question[questionPosition];
+
 
 // DECLARE the 'timerEl' variable
 var timerEl = document.querySelector("#timer");
@@ -55,20 +55,51 @@ function startTimer() {
     }, 1000);
 }
 
+var startButton = document.querySelector('#startQuiz');
+var startEl = document.querySelector('#start');
+
 // Function to 'startGame'
 function startGame() {
-    
-}
     // Hide the start screen
-
+    startEl.classList.add("hide");
+    // Show the Quiz page
+    questionEl.classList.remove("hide");
+    answersEl.classList.remove("hide");
     // Set the question position to 0
+    questionPosition = 0;
     // Display the current question
-        
+    displayCurrentQuestion();
+
     // Set the starting value of 'countdown'
+    // countdown = 75;
 
     // Start the timer calling the 'startTimer' function
+    startTimer();
 
-        // IF 'countdown' === ) THEN 'endGame()'
+        // IF 'countdown' === 0 THEN 'endGame()'
+    
+}
+
+startButton.addEventListener('click', startGame);
+
+// Grabs current question
+// var currentQuestion = questionArray[questionPosition];
+
+var answersEl = document.getElementById('answers');
+var questionEl = document.getElementbyId('question');
+
+// Function 'displayCurrentQuestion'
+function displayCurrentQuestion() {
+    let question = questionArray.question;
+
+    questionEl.textContent = questionArray.questionEl;
+
+    for (i = 0; i < question; i++) {
+        var currentQuestion = question[i];
+        var answerList = document.querySelector('#answer' + i);
+        answerList.textContent = currentQuestion;
+    }
+}
 
 // Function 'endGame'
 
@@ -98,7 +129,9 @@ function startGame() {
 
         // questionPosition++
 
-// Function 'displayCurrentQuestion'
+
+
+
 
 // Function 'recordHighScore'
 
